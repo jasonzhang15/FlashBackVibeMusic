563eb18935d3d411365e824bfa82976a7ad32475
mac <mac@macs-MacBook-Air.local>
2018-02-16 19:59:01 -0800

    .gitignore

--- flashbackmusic/app/src/main/java/com/android/flashbackmusic/MainActivity.java
+++ flashbackmusic/app/src/main/java/com/android/flashbackmusic/MainActivity.java
@@ -79,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
 
             SongBlock songBlock = new SongBlock(getApplicationContext(), song);
             songBlock.setText();
-
             songBlock.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
