be598159483823a0d416d9e0f3ba2dcfc8ea8e43
joswei <jsw033@ucsd.edu>
2018-03-11 13:25:45 -0700

    trying to write firebaseio

--- flashbackmusic/app/src/main/java/com/android/flashbackmusic/FirebaseIO.java
+++ flashbackmusic/app/src/main/java/com/android/flashbackmusic/FirebaseIO.java
@@ -1,10 +1,61 @@
 package com.android.flashbackmusic;
 
-import SongInfoIO;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by vrkumar on 3/1/18.
  */
 
 public class FirebaseIO implements SongInfoIO {
+    @Override
+    public void setup() {
+
+    }
+
+    public void sync() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myFirebaseRef = database.getReference();
+
+        Query queryRef = myFirebaseRef.orderByChild("studentId").equalTo(idToSearch);
+
+        queryRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot == null || snapshot.getValue() == null)
+                    Toast.makeText(MainActivity.this, "No record found", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Faile to read value
+                Log.w("TAG1", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+
+    @Override
+    public void populateSongInfo(Song s) {
+
+    }
+
+    @Override
+    public void storeSongInfo(Song s) {
+
+    }
+
+    @Override
+    public void teardown() {
+
+    }
 }
